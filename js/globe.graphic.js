1bf3cff7188928c702a840dc9165714c2040faff
Gabriel Florit <gabrielflorit@gmail.com>
2014-08-08 00:29:30 -0400

    think i fixed a mobile drawer edge case

--- js/globe.graphic.js
+++ js/globe.graphic.js
@@ -1,10 +1,12 @@
 globe.graphic = function() {
 
 	var master = $('#gf');
+
+	var graphicMobile;
 	
 	// if we're on touch, add the mobile template
 	if (Modernizr.touch) {
-		globe.graphicMobile($('.content', master), $('.subtitle, .source-and-credit', master));
+		graphicMobile = globe.graphicMobile($('.content', master), $('.subtitle, .source-and-credit', master));
 
 		if (navigator.userAgent.match(/iPad;.*CPU.*OS 7_\d/i)) {
 			$('html').addClass('ipad ios7');
@@ -34,156 +36,14 @@ globe.graphic = function() {
 		}
 	});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	
-
-	// var ExploreOnMyOwnControl = L.Control.extend({
-	// 	options: {
-	// 		position: 'bottomleft'
-	// 	},
-
-	// 	onAdd: function (map) {
-	// 		// create the control container with a particular class name
-	// 		var container = L.DomUtil.create('div', 'leaflet-bar explore-on-my-own');
-	// 		container.innerHTML = '<a href="#">Explore on my own</a>';
-
-	// 		L.DomEvent.addListener(container, 'click', function() {
-
-	// 			// collapse top drawer
-	// 			if ($('#gf .mobile-header .navicon').hasClass('minus')) {
-	// 				$('#gf .mobile-header .navicon').click();	
-	// 			}
-
-	// 			// remove "explore on my own"
-	// 			map.removeControl(exploreOnMyOwn);
-
-	// 			// remove "details"
-	// 			// map.removeControl(details);
-
-	// 			// is seeHotspots on the map? if not, add it
-	// 			debugger;
-
-	// 			// place "see hotspots" on top right
-	// 			seeHotspots.setPosition('topright');
-	// 		});
-
-	// 		return container;
-	// 	}
-	// });
-
-	// var SeeHotspotsControl = L.Control.extend({
-	// 	options: {
-	// 		position: 'bottomleft'
-	// 	},
-
-	// 	onAdd: function (map) {
-	// 		// create the control container with a particular class name
-	// 		var container = L.DomUtil.create('div', 'leaflet-bar see-hotspots');
-	// 		container.innerHTML = '<a href="#">See hotspots</a>';
-
-	// 		L.DomEvent.addListener(container, 'click', function() {
-
-	// 			// collapse top drawer
-	// 			if ($('#gf .mobile-header .navicon').hasClass('minus')) {
-	// 				$('#gf .mobile-header .navicon').click();	
-	// 			}
-
-	// 			// remove "explore on my own"
-	// 			map.removeControl(seeHotspots);
-
-	// 			// place "see hotspots" on top right
-	// 			exploreOnMyOwn.setPosition('topright');
-
-	// 			// add "details"
-	// 			map.addControl(details);
-	// 			details.update({
-	// 				town: 'Chelseass',
-	// 				text: 'Lorem ipsum Reprehenderit exercitation proident Excepteur eiusmod non ea voluptate in elit Duis labore magna irure in non pariatur pariatur officia velit tempor fugiat ex reprehenderit nostrud ea consectetur laborum.'
-	// 			});
-	// 		});
-
-	// 		return container;
-	// 	}
-	// });
-
-	// var DetailsControl = L.Control.extend({
-	// 	options: {
-	// 		position: 'bottomright'
-	// 	},
-
-	// 	onAdd: function (map) {
-	// 		// create the control container with a particular class name
-	// 		this._div = L.DomUtil.create('div', 'leaflet-bar details');
-
-	// 		return this._div;
-	// 	},
-
-	// 	update: function(props) {
-	// 		this._div.innerHTML = '<div class="town">' + props.town + '</div><div class="next">Next</div><div class="text">' + props.text + '</div>';
-	// 	}
-	// });
-
-	// var exploreOnMyOwn = new ExploreOnMyOwnControl();
-	// var seeHotspots = new SeeHotspotsControl();
-	// var details = new DetailsControl();
-
-	// map.addControl(exploreOnMyOwn);
-	// map.addControl(seeHotspots);
-
-	// $(master).on('click', '.leaflet-bar.details .next', function(e) {
-	// });
-
-
-	// map.addControl(new seeHotspots());
-
-
-
-
-
-
-
-
-
-
-
-	// var clusters = topojson.feature(globe.graphic.clusters, globe.graphic.clusters.objects.clusters);
-
-	// var clustersGroup = L.geoJson(clusters, {
-	// 	style: function (feature) {
-	// 		return {
-	// 			color: 'blue'
-	// 		}
-	// 	},
-	// 	onEachFeature: function (feature, layer) {
-	// 		layer.on('click', function(a, b, c, d) {
-	// 		});
-	// 	}
-	// });
-
-	// clustersGroup.addTo(map);
-
-	// var clusterIndex = 0;
-
-	// $('.begintour', master).click(function(e) {
-	// 	e.preventDefault();
-
-	// 	// navigate to next
-	// 	map.fitBounds(clustersGroup.getLayers()[clusterIndex++ % 5].getBounds());
-
-	// })
+	var fsm = StateMachine.create({
+		initial: 'splash',
+		events: [
+			{ name: 'warn',  from: 'green',  to: 'yellow' },
+			{ name: 'panic', from: 'yellow', to: 'red'    },
+			{ name: 'calm',  from: 'red',    to: 'yellow' },
+			{ name: 'clear', from: 'yellow', to: 'green'  }
+		]
+	});
 
 };
--- js/globe.graphicMobile.js
+++ js/globe.graphicMobile.js
@@ -11,33 +11,62 @@ globe.graphicMobile = function(bodyElements, drawerElements) {
 
 	$(bodyElements).appendTo('#gf .mobile-body');
 
-	$('#gf .mobile-header .navicon').click(function(e) {
+	$('#gf').attr('data-drawer', 'expanded');
 
-		// is the drawer collapsed?
-		if (!$(this).hasClass('minus')) {
+	function expandDrawer() {
 
-			// the header has a black bottom border
-			// immediately make the border transparent
-			$('#gf .mobile-header').addClass('expanded');
+		// notice we make border transparent immediately,
+		// and after the navicon is done transitioning, again
+		// this is to cover the edge case of clicking collapse-expand
+		// without waiting for collapse to finish
 
+		// the header has a black bottom border
+		// immediately make the border transparent
+		$('#gf .mobile-header .navicon').bind('transitionend oTransitionEnd webkitTransitionEnd', function(e) {
+
+			$('#gf .mobile-header').addClass('expanded'); // make bottom border transparent
+			$('#gf .mobile-header .navicon').unbind();
+		});
+
+		$('#gf .mobile-header').addClass('expanded'); // make bottom border transparent
+		$('#gf .mobile-drawer').addClass('expanded'); // translate drawer down
+		$('#gf .mobile-header .navicon').addClass('minus'); // animate 3 lines to minus
+
+		$('#gf').attr('data-drawer', 'expanded');
+	}
+
+	function collapseDrawer() {
+
+		// the header has a white bottom border
+		// wait until the drawer has collapsed,
+		// then make the border transparent
+		$('#gf .mobile-header .navicon').bind('transitionend oTransitionEnd webkitTransitionEnd', function(e) {
+
+			$('#gf .mobile-header').removeClass('expanded'); // make bottom border black
+			$('#gf .mobile-header .navicon').unbind();
+		});
+
+		$('#gf .mobile-drawer').removeClass('expanded'); // translate drawer up
+		$('#gf .mobile-header .navicon').removeClass('minus'); // animate minus to 3 lines
+
+		$('#gf').attr('data-drawer', 'collapsed');
+	}
+
+	function toggleDrawer() {
+
+		if ($('#gf').attr('data-drawer') === 'expanded') {
+			collapseDrawer();
 		} else {
-
-			// the drawer is expanded
-			// the header has a white bottom border
-			// wait until the drawer has collapsed,
-			// then make the border transparent
-			$('#gf .mobile-header .navicon').bind('transitionend oTransitionEnd webkitTransitionEnd', function(e) {
-
-				$('#gf .mobile-header').removeClass('expanded');
-				$('#gf .mobile-header .navicon').unbind();
-
-			});
-
+			expandDrawer();
 		}
 
-		$('#gf .mobile-drawer').toggleClass('expanded');
-		$(this).toggleClass('minus');
+	}
 
-	});
+	$('#gf .mobile-header .navicon').click(toggleDrawer);
+
+	return {
+		expandDrawer: expandDrawer,
+		collapseDrawer: collapseDrawer
+	};
 
 };
